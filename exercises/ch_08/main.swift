49d10bc535d3ac379374e3986b6a66656f914cb7
Piotr Fulmański <piotr@fulmanski.pl>
2022-11-22 09:18:31 +0100

    Add more comments

--- exercises/ch_08/main.swift
+++ exercises/ch_08/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // === Email
-// Define fields
+// Define type of fields used to define any item in email group
 var emailFields = [FieldType]()
 emailFields.append(.text(name: "Name", required: true))
 emailFields.append(.email(name: "Email", required: true))
@@ -16,10 +16,11 @@ emailFields.append(.text(name: "Password"))
 
 // Create a group to store informations about emails
 var groupEmails = Group(name: "Emails")
+// Save informations about fields which define any item in this group
 groupEmails.fieldType = emailFields
 
 // === Credit cards
-// Define fields
+// Define type of fields used to define any item in credit cards group
 var creditCardFields = [FieldType]()
 creditCardFields.append(.text(name: "Name", required: true))
 creditCardFields.append(.text(name: "Number", required: true))
@@ -29,20 +30,27 @@ creditCardFields.append(.text(name: "Valid trough"))
 
 // Create a group to store informations about credit cards
 var groupCreditCards = Group(name: "Credit cards")
+// Save informations about fields which define any item in this group
 groupCreditCards.fieldType = creditCardFields
 
 // Get some data
 
 print("=== Informations about my email accounts:")
 
+// Create an empty item from group emails
 var emailPrivate = groupEmails.getItem()
+// Ask user for input data
 emailPrivate.getData()
+// Save data provided by user in group emails
 groupEmails.storeData(emailPrivate)
 
 print("=== Informations about my credit cards:")
 
+// Create an empty item from group credit cards
 var credtCardVisa = groupCreditCards.getItem()
+// Ask user for input data
 credtCardVisa.getData()
+// Save data provided by user in group credit cards
 groupCreditCards.storeData(credtCardVisa)
 
 // Print data
